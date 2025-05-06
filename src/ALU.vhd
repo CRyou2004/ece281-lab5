0cf07c27029cb05ae7caa42b951e54f4b57db483
CRyou2004 <Chaimin2004@gmail.com>
2025-05-05 20:27:44 -0600

    PLSSS

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -29,7 +29,7 @@ entity ALU is
           i_B : in STD_LOGIC_VECTOR (N-1 downto 0);
           o_flag : out STD_LOGIC_VECTOR (2 downto 0);
           o_ALU : out STD_LOGIC_VECTOR (N-1 downto 0);
-          op : in STD_LOGIC_VECTOR (2 downto 0));
+          i_op : in STD_LOGIC_VECTOR (2 downto 0));
 end ALU;
 
 architecture behavioral of ALU is 
@@ -43,14 +43,14 @@ signal w_add    : STD_LOGIC_VECTOR (N downto 0);
   
 begin
 	-- PORT MAPS ----------------------------------------
-    w_cin <= op(0 downto 0);
+    w_cin <= i_op(0 downto 0);
     
-    w_B <= not i_B when op = "001" else i_B;
+    w_B <= not i_B when i_op = "001" else i_B;
     w_add(N-1 downto 0) <= std_logic_vector(unsigned(i_A) + unsigned(w_B) + unsigned(w_cin));
     
    -- w_shift <= std_logic_vector(shift_left(unsigned(i_A), to_integer(unsigned(i_B(2 downto 0))))) when op(0) = '0';
    
-   o_ALU <= w_add(N-1 downto 0) when op(2 downto 1) = "00";
+   o_ALU <= w_add(N-1 downto 0) when i_op(2 downto 1) = "00";
    
    o_flag(0) <= w_add(N);
    o_flag(1) <= '1' when w_ALUout = "00000000" else '0';
