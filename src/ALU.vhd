57d50f78f99ef8d19e1b688638058d3a62b563f4
CRyou2004 <Chaimin2004@gmail.com>
2025-05-05 20:22:16 -0600

    PLS

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -21,60 +21,45 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
+
 entity ALU is
-    Port ( i_regA : in std_logic_vector(7 downto 0);
-           i_regB : in std_logic_vector(7 downto 0);
-           i_op : in std_logic_vector(2 downto 0);
-           o_result : out std_logic_vector(7 downto 0);
-           o_flags : out std_logic_vector(2 downto 0));
+-- TODO
+    generic(N: integer := 8);
+    Port (i_A : in STD_LOGIC_VECTOR (N-1 downto 0);
+          i_B : in STD_LOGIC_VECTOR (N-1 downto 0);
+          o_flag : out STD_LOGIC_VECTOR (2 downto 0);
+          o_ALU : out STD_LOGIC_VECTOR (N-1 downto 0);
+          op : in STD_LOGIC_VECTOR (2 downto 0));
 end ALU;
 
 architecture behavioral of ALU is 
+  
 	-- declare components and signals
-	--components
-	component fullAdder is
-	Port(i_A : in std_logic_vector(7 downto 0);
-	     i_B : in std_logic_vector(7 downto 0);
-         i_Cin : in std_logic;
-         o_S : out std_logic_vector(7 downto 0);
-         o_Cout : out std_logic);
-    end component;
-
-    --signals
-    signal w_sum : std_logic_vector(7 downto 0);
-    signal w_B : std_logic_vector(7 downto 0);
-    signal w_shifter : std_logic_vector(7 downto 0);
-    signal w_opcode : std_logic_vector(2 downto 0);
-    signal w_bitwise : std_logic_vector(7 downto 0);
-    
+signal w_B      : STD_LOGIC_VECTOR (N-1 downto 0);
+signal w_ALUout : STD_LOGIC_VECTOR (N-1 downto 0);
+signal w_cin    : STD_LOGIC_VECTOR (0 downto 0);
+signal w_cout   : STD_LOGIC;
+signal w_add    : STD_LOGIC_VECTOR (N downto 0);
+  
 begin
 	-- PORT MAPS ----------------------------------------
+    w_cin <= op(0 downto 0);
+    
+    w_B <= not i_B when op = "001" else i_B;
+    w_add(N-1 downto 0) <= std_logic_vector(unsigned(i_A) + unsigned(w_B) + unsigned(w_cin));
+    
+   -- w_shift <= std_logic_vector(shift_left(unsigned(i_A), to_integer(unsigned(i_B(2 downto 0))))) when op(0) = '0';
+   
+   o_ALU <= w_add(N-1 downto 0) when op(2 downto 1) = "00";
+   
+   o_flag(0) <= w_add(N);
+   o_flag(1) <= '1' when w_ALUout = "00000000" else '0';
+   o_flag(2) <= w_ALUout(N-1);
 	
-	fullAdder_inst : fullAdder
-	    Port map(
-	    i_A => i_regA,
-	    i_B => w_B,
-	    i_Cin => i_op(2), --just looks at MSB of i_op
-	    o_Cout => o_flags(0), --double check order of o_flags	    
-	    o_S => w_sum
-	    );
-	   
-	    --2:1 Mux for i_B inside add/sub component
-	    w_B <= not(i_regB) when(i_op(2) = '1')
-	           else i_regB;
-	   
-	   --2:1 Mux for to choose AND or OR inside bitwise AND/OR component
-       w_bitwise <= i_regA or i_regB when i_op(2) = '1' else
-                    i_regA and i_regB;
-	   
-	   --Shifter logic
-	   w_shifter <= std_logic_vector(unsigned(i_regA) sll to_integer(unsigned(i_regB(2 downto 0)))) when i_op(2) = '0' else 
-	               std_logic_vector(unsigned(i_regA) srl to_integer(unsigned(i_regB(2 downto 0))));
-	   
-	   o_result <= w_sum when i_op(1 downto 0) = "00" else
-	               w_bitwise when i_op(1 downto 0) = "10" else
-	               w_shifter when i_op(1 downto 0) = "01" else
-	               "00000000" when i_op(1 downto 0) = "11";
-	               
+	
+	-- CONCURRENT STATEMENTS ----------------------------
+	
+	
+	
 end behavioral;
 
--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -21,44 +21,40 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+ 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
-
+ 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
+ 
 entity controller_fsm is
-  Port (i_reset : in std_logic;
-        i_adv   : in std_logic;
-        i_clk   : in std_logic;
-        o_cycle : out std_logic_vector(3 downto 0)
-   );
+    Port ( i_reset : in STD_LOGIC;
+           i_adv : in STD_LOGIC;
+           o_cycle : out STD_LOGIC_VECTOR (3 downto 0));
 end controller_fsm;
-
-architecture Behavioral of controller_fsm is
-
-    signal f_Q, f_Q_next : std_logic_vector(3 downto 0);
-    constant k_clk_period : time := 10ns;
-    
+ 
+architecture FSM of controller_fsm is
+ 
+	-- create register signals with default state clear (0001)
+	signal f_Q : std_logic_vector(3 downto 0) := "0001";
+	signal f_Q_next : std_logic_vector(3 downto 0) := "0001";
+ 
 begin
-    f_Q_next <= "1000" when (i_reset = '1' or (f_Q = "0100" and i_adv = '1')) else
-                "0001" when (i_adv = '1' and f_Q = "1000") else
-                "0010" when (i_adv = '1' and f_Q = "0001") else
-                "0100" when (i_adv = '1' and f_Q = "0010") else
-                "1000";
-    o_cycle <= f_Q;
-    
-    register_proc : process (i_clk, i_reset) 
-            begin
-                if i_reset = '1' then
-                    f_Q <= "1000";        -- reset state is yellow
-                elsif (rising_edge(i_clk) and i_adv='1') then
-                    f_Q <= f_Q_next;    -- next state becomes current state
-                end if;
-            end process register_proc;
-
-end Behavioral;
+ 
+ 
+    --Next state logic
+    f_Q_next(3) <= f_Q(2) and i_adv;
+    f_Q_next(2) <= f_Q(1) and i_adv;
+    f_Q_next(1) <= f_Q(0) and i_adv;
+    f_Q_next(0) <= (f_Q(0) and i_reset) or (f_Q(1) and i_reset) or (f_Q(2) and i_reset) or (f_Q(3) and i_reset) or (f_Q(3) and i_adv);
+    --Output logic
+    o_cycle(3) <= f_Q(3);
+    o_cycle(2) <= f_Q(2);
+    o_cycle(1) <= f_Q(1);
+    o_cycle(0) <= f_Q(0);
+ 
+end FSM;
